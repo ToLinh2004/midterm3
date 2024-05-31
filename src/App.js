eb01f3d5303a1d0eebf6c7afcb5056009e2d9681
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-31 17:12:42 +0700

    changle theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+const handleThem= ()=>{
+  setIsDarkMode(!isDarkMode);
+}
   return (
-    <div className="App">
+    <div className="App"  style={{ 
+      backgroundColor: isDarkMode ? 'black' : 'white',
+    }}>
       <Router>
         <Navbar />
+      <button onClick={handleThem} style={{borderRadius:'10px',height:'40px',width:'100px',marginRight:'80%'}}>{isDarkMode ? 'Light' : 'Dark'}</button>
         <Home />
       </Router>
     </div>
